cb3e00fa50657837184472ab2b18c687cde260e8
Nick Stuivenberg <n.stuivenberg@novi.nl>
2020-07-21 08:34:00 +0200

    Added javadoc with explanation for students.

--- src/main/java/nl/novi/dbexample/exception/DogNotFoundAdvice.java
+++ src/main/java/nl/novi/dbexample/exception/DogNotFoundAdvice.java
@@ -9,6 +9,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class DogNotFoundAdvice {
 
+    /**
+     * Met ResponseBody geven we aan dat we een naar JSON geserialized object teruggeven. Dit object wordt in een
+     * HTTPResponse gezet.
+     *
+     * De ExceptionHandler moet een verwijzing bevatten naar de Exceptie die we gemaakt hebben. Let op, hier moet class
+     * achter.
+     *
+     * Met ResponseStatus geven we aan welke HTTP status we willen meegeven. HTTP 404 is zo'n status. De 404 betekent
+     * not found. Je kunt ze hier allemaal vinden:
+     * https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/http/HttpStatus.html
+     * @param ex De exceptie.
+     * @return The error message.
+     */
     @ResponseBody
     @ExceptionHandler(DogNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
--- src/main/java/nl/novi/dbexample/exception/DogNotFoundException.java
+++ src/main/java/nl/novi/dbexample/exception/DogNotFoundException.java
@@ -2,6 +2,10 @@ package nl.novi.dbexample.exception;
 
 public class DogNotFoundException extends RuntimeException {
 
+    /**
+     * Creates the DogNotFoundException
+     * @param id of the dog that was not found in the database
+     */
     public DogNotFoundException(Long id) {
         super("Could not find dog with id: " + id);
     }
